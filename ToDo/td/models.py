f5f6ec8bee894ba508db1aef600e020e6627551b
Sandro <sandrocoelho2023a@gmail.com>
2025-08-22 19:12:57 -0300

    criando o modelo de tarefas

--- ToDo/td/models.py
+++ ToDo/td/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Tarefas(models.Model):
+    autorTarefa = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tareafas_por_autor")
+    dataTarefa = models.DateField()
+    LocalTarefa = models.CharField(max_length=64)
+    descricaoTarefa = models.CharField(max_length=1000)
+    tarefa = models.CharField(max_length=64)
+    objetivoTarefa = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.autorTarefa} - {self.tarefa}"
